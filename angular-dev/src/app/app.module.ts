4de6561a19aa16a54cd50785f350bb611431a46b
Noah Cohen <nbcohen@bu.edu>
2019-11-12 22:10:08 -0500

    oauth without backend

--- angular-dev/src/app/app.module.ts
+++ angular-dev/src/app/app.module.ts
@@ -7,6 +7,22 @@ import { AppComponent } from './app.component';
 import { SearchComponent } from './search/search.component';
 import {HttpClientModule} from '@angular/common/http';
 import { LoginComponent } from './login/login.component';
+import { SocialLoginModule, AuthServiceConfig, AuthService, GoogleLoginProvider} from 'angularx-social-login';
+
+// Configs 
+export function getAuthServiceConfigs() {
+  const ClientID = "991940894202-2fiu3ivcpj8p0t1d03rjcu10qvcslira.apps.googleusercontent.com"
+  let config = new AuthServiceConfig(
+  [
+    {
+      id: GoogleLoginProvider.PROVIDER_ID,
+      provider: new GoogleLoginProvider(ClientID),
+    }
+  ]);
+ return config;
+}
+
+
 
 @NgModule({
   declarations: [
@@ -18,8 +34,14 @@ import { LoginComponent } from './login/login.component';
     BrowserModule,
     HttpClientModule,
     RouterModule.forRoot(routes),
+    SocialLoginModule,
   ],
-  providers: [],
+  providers: [
+    {
+      provide: AuthServiceConfig,
+      useFactory: getAuthServiceConfigs,
+    }
+  ],
   bootstrap: [ AppComponent ]
 })
 export class AppModule { }
--- angular-dev/src/app/login/login.component.ts
+++ angular-dev/src/app/login/login.component.ts
@@ -1,17 +1,35 @@
 import { Component, OnInit } from '@angular/core';
-import { RouterLink, Router } from '@angular/router';
+import { Router } from '@angular/router';
+import { AuthService, GoogleLoginProvider, SocialLoginModule } from 'angularx-social-login';
 
 @Component({
   selector: 'app-login',
+  providers: [AuthService, SocialLoginModule],
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
 
-  constructor(private router: Router,) { }
+  constructor(
+    private router: Router,
+    private socialAuthService: AuthService,
+    ) { }
 
   ngOnInit() {
   }
+
+  public signinWithGoogle () {
+    let socialPlatformProvider = GoogleLoginProvider.PROVIDER_ID;
+ 
+    this.socialAuthService.signIn(socialPlatformProvider)
+    .then((userData) => {
+       //on success
+       //this will return user data from google. What you need is a user token which you will send it to the server
+
+       //this.sendToRestApiMethod(userData.idToken);
+       console.log(userData);
+    });
+ }
 
 
   login() {
