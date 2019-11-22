9fc8b13d441f0b85d8d881347de12e0a9a152460
Wyatt Howe <wyatt@wyatthowe.net>
2019-11-22 02:50:41 -0500

    oath backend support, bug fixes

--- angular-dev/src/app/login/login.component.ts
+++ angular-dev/src/app/login/login.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService, GoogleLoginProvider, SocialLoginModule } from 'angularx-social-login';
+import { HttpClient, HttpHeaders, HttpParams } from './../../../node_modules/@angular/common/http';
+
 
 @Component({
   selector: 'app-login',
@@ -12,28 +14,58 @@ export class LoginComponent implements OnInit {
 
   constructor(
     private router: Router,
+    public redirect: Router,
     private socialAuthService: AuthService,
-    ) { }
+    private http: HttpClient,
+  ) { }
 
   ngOnInit() {
   }
 
   public signinWithGoogle () {
     let socialPlatformProvider = GoogleLoginProvider.PROVIDER_ID;
- 
+
     this.socialAuthService.signIn(socialPlatformProvider)
     .then((userData) => {
-       //on success
-       //this will return user data from google. What you need is a user token which you will send it to the server
+      //on success
+      //this will return user data from google. What you need is a user token which you will send it to the server
 
-       //this.sendToRestApiMethod(userData.idToken);
-       console.log(userData);
+      this.sendToRestApiMethod(userData.idToken);
+      console.log(userData);
     });
- }
+  }
 
 
   login() {
-    this.router.navigate(['/search'])
+    setTimeout(function(){
+      this.redirect.navigate(['/search']);
+    }.bind(this), 2000);
+
+  }
+
+  sendToRestApiMethod(idToken) {
+    console.log('Verifying token...');
+
+    // handle promise when resolved
+    function f (result : {authentic: Boolean, token: String}) {
+      console.log('auth response: ', result.authentic);
+      if (result.authentic) {
+        console.log('OAuth success.\nRedirecting...');
+        document.getElementById("multi_lines_text").innerHTML = '<p">Redirecting...<p>';
+        // (new LoginComponent).login();
+        this.login();
+      } else {
+        console.log('Invalid username or password.');
+        document.getElementById("multi_lines_text").innerHTML = '<p style="color: red">Authorization failed<p>';
+      }
+    };
+
+    // check that server trusts us
+    this.http.get("http://54.164.165.203/oauth.php", {
+      params: {
+        "idToken": idToken
+      }
+    }).subscribe(f.bind(this), f.bind(this));
   }
 
 }
